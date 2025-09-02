1124f6669d090bdd3eb37c0dbf2bb13ddc823366
Manishkashyap1803 <manishkashyap8479@gmail.com>
2025-09-02 22:35:16 +0530

    pushing all my code

--- controllers/Usercontrollers.js
+++ controllers/Usercontrollers.js
@@ -1,50 +1,56 @@
 const User = require('../models/User');
-const Student = require('../models/Student')
-const bcrypt = require('bcrypt')
- async function addUser(req,res){
-    try{
-        console.log(req.body,'req.body');
+const Student = require('../models/Student');
+const bcrypt = require('bcrypt');
+
+async function addUser (req, res) {
+    try {
+        console.log(req.body, 'req.body');
         let user = new User(req.body);
-       
-        let encryptredPassword = bcrypt.hashSync(req.body.Password, 10);
-        console.log(encryptredPassword, 'encryptredPassword')
-        user.Password = encryptredPassword;
+
+        let encryptedPassword = bcrypt.hashSync(req.body.Password, 10);
+        console.log(encryptedPassword, 'encryptedPassword');
+        user.Password = encryptedPassword;
+
         await user.save();
-        console.log("data is save is successfully");
+        console.log("Data is saved successfully");
         res.redirect('/');
-        
-    }
-    catch (err){
-        console.log(err);
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
     }
 }
- async function dologin(req,res){
-    try{
-        console.log(req.body,'req.body');
-        let user = await User.findOne({Email: req.body.Email});
-        console.log('user',user);
-        if(user){
 
-            let validPassword =  await bcrypt.compare(req.body.Password,user.Password);
-            if(validPassword){
-                if(user.usertype==='Admin')
-                let students = await Student.find({});
-                res.render('welcomeadmin',{
-                    students: students
-                });
-            }else{
-                res.end('<h1> invalid email/password.............')
+async function dologin(req, res) {
+    try {
+        console.log(req.body, 'req.body');
+        let user = await User.findOne({ Email: req.body.Email });
+        console.log('user', user);
+
+        if (user) {
+            let validPassword = await bcrypt.compare(req.body.Password, user.Password);
+            if (validPassword) {
+                if (user.usertype === 'Admin') {
+                    let students = await Student.find({});
+                    return res.render('welcomeadmin', {
+                        students: students
+                    });
+                } else {
+                    // You can add handling for other user types here
+                    return res.send('<h1>Welcome user</h1>');
+                }
+            } else {
+                return res.status(401).send('<h1>Invalid email/password</h1>');
             }
+        } else {
+            return res.status(404).send('<h1>User does not exist</h1>');
         }
-        else{
-            res.end("<h1 user does not exsists.................>")
-        }
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Internal Server Error');
     }
-    catch (err){
+}
 
-    }
-}
 module.exports = {
-    addUser,
+    addUser ,
     dologin
-}
+};
